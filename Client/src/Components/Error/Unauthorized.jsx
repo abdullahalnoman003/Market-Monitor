4d97b46ca00dd2ad302fb7057254584485913fa7
abdullahalnoman003 <noman22622@gmail.com>
2025-07-13 03:26:24 +0600

    Added Unauthorized page

---
+++ Client/src/Components/Error/Unauthorized.jsx
@@ -0,0 +1,9 @@
+const Unauthorized = () => {
+  return (
+    <div className="h-screen flex items-center justify-center font-bold text-center text-red-500 text-xl">
+       <h1 > <span className="text-4xl">⚠️ Warning ⚠️</span> <br />You are not authorized to view this page.</h1>
+    </div>
+  );
+};
+
+export default Unauthorized;
