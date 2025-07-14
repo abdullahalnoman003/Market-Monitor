23f8790b213abdd3b76c29fbefd15ebffe66f3b7
abdullahalnoman003 <noman22622@gmail.com>
2025-07-15 02:24:01 +0600

    Using Axios Secure And Interceptor, for Verify API

--- Client/src/Hooks/useAxiosSecure.jsx
+++ Client/src/Hooks/useAxiosSecure.jsx
@@ -1,7 +1,41 @@
-import axios from "axios";
+import axios from 'axios';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Components/Authentication/Context/AuthContext';
 
-const useAxiosSecure = axios.create({
-  baseURL: `http://localhost:3000/`,
-});
+const axiosSecure = axios.create({
+    // baseURL: `https://monitor-market-server.vercel.app/`
+        baseURL: `http://localhost:3000/`
+
+})
+const useAxiosSecure = () => {
+    const { user, logOut } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    axiosSecure.interceptors.request.use(config => {
+        config.headers.Authorization = `Bearer ${user.accessToken}`
+        return config;
+    }, error => {
+        return Promise.reject(error);
+    })
+
+    axiosSecure.interceptors.response.use(res => {
+        return res;
+    }, error => {
+        const status = error.status;
+        if (status === 403) {
+            navigate('/forbidden');
+        }
+        else if (status === 401) {
+            logOut()
+                .then(() => {
+                    navigate('/login')
+                })
+                .catch(() => { })
+        }
+        return Promise.reject(error);
+    })
+    return axiosSecure;
+};
 
 export default useAxiosSecure;
