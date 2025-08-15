c9cfa539173716c7d90b33d24077fafe38769417
abdullahalnoman003 <noman22622@gmail.com>
2025-08-16 01:50:57 +0600

    Updated Package

--- Client/src/Components/Layout/Home/ProductSection.jsx
+++ Client/src/Components/Layout/Home/ProductSection.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import useAxios from "../../../Hooks/useAxios";
 
 const ProductSection = () => {
--- Client/src/Components/Layout/PublicLayout/ProductDetails.jsx
+++ Client/src/Components/Layout/PublicLayout/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router";
+import { useNavigate, useParams } from "react-router-dom";
 import { FaRegBookmark } from "react-icons/fa";
 import { FaCartPlus } from "react-icons/fa6";
 import Swal from "sweetalert2";
